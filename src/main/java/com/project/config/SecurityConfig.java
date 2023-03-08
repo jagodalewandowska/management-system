d787c126062fb4d25ad6ecce3192634b2e2197d1
jagodalewandowska <jadlewandowska@gmail.com>
2023-03-08 18:02:54 +0100

    Added SecurityConfig

--- src/main/java/com/project/config/SecurityConfig.java
+++ src/main/java/com/project/config/SecurityConfig.java
@@ -6,12 +6,11 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-    
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
-            .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-            .httpBasic();
-    return http.build();
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .httpBasic();
+        return http.build();
     }
 }
