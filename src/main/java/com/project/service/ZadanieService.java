2e9e1074514178944037aa192b39be61e35b8beb
jagodalewandowska <jadlewandowska@gmail.com>
2023-03-08 17:36:44 +0100

    Added empty files - todo

--- src/main/java/com/project/service/ZadanieService.java
+++ src/main/java/com/project/service/ZadanieService.java
@@ -1,4 +1,4 @@
 package com.project.service;
 
-public class ZadanieService {
+public interface ZadanieService {
 }
--- src/main/java/com/project/service/ZadanieServiceImpl.java
+++ src/main/java/com/project/service/ZadanieServiceImpl.java
@@ -1,4 +1,4 @@
 package com.project.service;
 
-public interface ZadanieServiceImpl {
+public class ZadanieServiceImpl {
 }
