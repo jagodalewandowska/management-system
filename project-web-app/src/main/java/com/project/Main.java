9c3989e87fa78bba2acccedff43c48a040ffae24
Maciej Szerwiński <maszymika1999@wp.pl>
2023-03-14 13:45:50 +0100

    Fixed Main.java file. Added open file option using spring.

--- project-web-app/src/main/java/com/project/Main.java
+++ project-web-app/src/main/java/com/project/Main.java
@@ -1,7 +1,12 @@
 package com.project;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class, args);
     }
 }
