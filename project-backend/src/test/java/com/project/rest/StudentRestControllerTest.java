3cd58d6d94b87d11071d013cd4e0a725b9e632c0
blaszu000 <bjsz1998@wp.pl>
2023-04-26 09:23:11 +0200

    Testy

---
+++ project-backend/src/test/java/com/project/rest/StudentRestControllerTest.java
@@ -0,0 +1,61 @@
+package com.project.rest;
+
+import com.project.model.Projekt;
+import com.project.model.Student;
+import com.project.service.ProjektService;
+import com.project.service.StudentService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Collections;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@WithMockUser(username = "admin", password = "admin")
+public class StudentRestControllerTest {
+    private final String apiPath = "/api/studenci";
+    @MockBean
+    private StudentService mockStudentService;
+    @Autowired
+    private MockMvc mockMvc;
+    private JacksonTester<Projekt> jacksonTester;
+
+    @Test
+    public void getStudenci() throws Exception {
+        Student student= new Student("Jan", "Kowalski", "12345", "kowalskijan@gmail.com", true);
+        Page<Student> page = new PageImpl<>(Collections.singletonList(student));
+        when(mockStudentService.getStudenci(any(Pageable.class))).thenReturn(page);
+        mockMvc.perform(get(apiPath).contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content[*]").exists()) //content[*] - oznacza całą zawartość tablicy content
+                .andExpect(jsonPath("$.content[0].imie").value(student.getImie()))
+                .andExpect(jsonPath("$.content[0].nazwisko").value(student.getNazwisko()))
+                .andExpect(jsonPath("$.content[0].nrIndeksu").value(student.getNrIndeksu()))
+                .andExpect(jsonPath("$.content[0].email").value(student.getEmail()))
+                .andExpect(jsonPath("$.content[0].stacjonarny").value(student.getStacjonarny()));
+
+        verify(mockStudentService, times(1)).getStudenci(any(Pageable.class));
+        verifyNoMoreInteractions(mockStudentService);
+    }
+}
