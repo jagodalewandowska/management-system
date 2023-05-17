c4631fc78d1d635af764b8f1e3b9a13601bddcd7
jagodalewandowska <jadlewandowska@gmail.com>
2023-05-17 09:10:24 +0200

    SecurityWebConfig & fixed errors

--- project-backend/src/main/java/com/project/config/JwtService.java
+++ project-backend/src/main/java/com/project/config/JwtService.java
@@ -4,7 +4,8 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
-import lombok.Value;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
--- project-backend/src/main/java/com/project/model/Role.java
+++ project-backend/src/main/java/com/project/model/Role.java
@@ -1,5 +1,5 @@
 package com.project.model;
 
 public enum Role {
-    Admin, User
+    ADMIN, USER
 }
