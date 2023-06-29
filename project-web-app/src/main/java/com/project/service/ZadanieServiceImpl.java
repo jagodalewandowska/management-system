9379067b4f7a6485329d9ea85e9427f723b5a81c
jagodalewandowska <jadlewandowska@gmail.com>
2023-06-29 14:26:04 +0200

    icon fix

--- project-web-app/src/main/java/com/project/service/ZadanieServiceImpl.java
+++ project-web-app/src/main/java/com/project/service/ZadanieServiceImpl.java
@@ -66,13 +66,10 @@ public class ZadanieServiceImpl implements ZadanieService {
     }
     @Override
     public void deleteZadanie(Integer zadanieId, Integer projektId) {
-        URI url = ServiceUtil.getUriComponent(serverUrl, getResourcePath(zadanieId) + getProjectPath(projektId))
-                .build()
-                .toUri();
+        String url = serverUrl + getResourcePath(zadanieId) + getProjectPath(projektId);
         logger.info("REQUEST -> DELETE {}", url);
         restTemplate.delete(url);
     }
-
     private Page<Zadanie> getPage(URI uri, RestTemplate restTemplate) {
         return ServiceUtil.getPage(uri, restTemplate,
                 new ParameterizedTypeReference<RestResponsePage<Zadanie>>() {});
